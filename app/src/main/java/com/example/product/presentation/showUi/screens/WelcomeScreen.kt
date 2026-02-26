cf11d8bc2a0f4554d50f9239f47422210540f459
Ashot-Android-Dev <ashotb750@gmail.com>
2026-02-26 22:58:54 +0400

    Commit third message

---
+++ app/src/main/java/com/example/product/presentation/showUi/screens/WelcomeScreen.kt
@@ -0,0 +1,96 @@
+package com.example.product.presentation.showUi.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation3.runtime.NavKey
+import com.example.product.R
+import com.example.product.presentation.components.CustomButton
+import com.example.product.presentation.components.CustomImage
+import com.example.product.presentation.navigation.route.HomeDataScreen
+import com.example.product.ui.theme.BackgroundColor
+import com.example.product.ui.theme.PrimaryColor
+import com.example.product.ui.theme.PrimaryVariantColor
+import com.example.product.ui.theme.TitleTextStyle
+
+@Composable
+fun WelcomeScreen(
+    modifier: Modifier = Modifier,
+    onNavigate: (NavKey) -> Unit = {}
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(BackgroundColor)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.6f)
+        ) {
+            CustomImage(
+                image = R.drawable.shopingimage,
+            )
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .windowInsetsPadding(WindowInsets.navigationBars)
+                .clip(shape = RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp))
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            PrimaryColor,
+                            PrimaryVariantColor
+                        )
+                    )
+                )
+                .padding(32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = stringResource(R.string.title),
+                style = TitleTextStyle.copy(
+                    color = Color.White,
+                    textAlign = TextAlign.Center,
+                ),
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            CustomButton(
+                str = R.string.start,
+                fontSize = 25.sp,
+                containerColor = Color.White,
+                contentColor = PrimaryVariantColor
+            ) {
+                onNavigate(HomeDataScreen)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun WelcomeScreenPreview() {
+    WelcomeScreen()
+}
